f76a2d3215f548de8ce10680b9d70f4935a4e3cd
MrBrainowner <kenthendico011@gmail.com>
2025-05-03 00:32:40 +0800

    marquee

--- src/sections/Gallery.jsx
+++ src/sections/Gallery.jsx
@@ -3,9 +3,23 @@ import "../index.css";
 
 const Gallery = () => {
   return (
-    <section className="p-5 w-full h-screen">
+    <section className="w-full h-screen">
       <div className="bg-retro-red relative w-full">
-        <div className="p-3 flex flex-row justify-between gap-5">
+        <div className="marquee p-3 flex flex-row justify-between">
+          <p>Fashion is about confidence wear what makes you feel great!</p>
+          <p>
+            Embrace comfort without compromising on style find your perfect
+            balance.
+          </p>
+          <p>Perfect fit over fleeting trends good tailoring is timeless.</p>
+          <p>A great pair of shoes can elevate even the simplest outfit.</p>
+          <p>
+            Less is more use accessories to enhance your look, not overpower it.
+          </p>
+          <p>
+            Mix different textures like leather, wool, and cotton to add
+            interest to your outfits.
+          </p>
           <p>Fashion is about confidence wear what makes you feel great!</p>
           <p>
             Embrace comfort without compromising on style find your perfect
