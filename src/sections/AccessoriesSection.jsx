7b2184cb973f39e94c43a02ce79d2babe35bf8ee
MrBrainowner <kenthendico011@gmail.com>
2025-05-06 00:24:06 +0800

    99%

--- src/sections/AccessoriesSection.jsx
+++ src/sections/AccessoriesSection.jsx
@@ -10,57 +10,57 @@ const AccessoriesSection = () => {
         />
         <img
           className="w-full size-40 object-cover"
-          src="https://images.unsplash.com/photo-1668906093328-99601a1aa584?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80"
+          src="https://i.pinimg.com/736x/32/d9/57/32d957cfd076a7bdf4736ad3a9ecc770.jpg"
           alt="Gallery Image"
         />
         <img
           className="w-full size-40 object-cover"
-          src="https://images.unsplash.com/photo-1567016526105-22da7c13161a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80"
+          src="https://i.pinimg.com/736x/33/ce/2b/33ce2bfc2b27bc986759b5c6c6db0d6f.jpg"
           alt="Gallery Image"
         />
         <img
           className="w-full size-40 object-cover"
-          src="https://images.unsplash.com/photo-1668584054131-d5721c515211?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80"
+          src="https://i.pinimg.com/736x/85/22/ef/8522efd608027f72346172cf8e59d90d.jpg"
           alt="Gallery Image"
         />
         <img
           className="w-full size-40 object-cover"
-          src="https://images.unsplash.com/photo-1664574654529-b60630f33fdb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80"
+          src="https://i.pinimg.com/736x/6c/49/49/6c49494130c2513e2b545ba28614b464.jpg"
           alt="Gallery Image"
         />
         <img
           className="w-full size-40 object-cover"
-          src="https://images.unsplash.com/photo-1669824774762-65ddf29bee56?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80"
+          src="https://i.pinimg.com/736x/11/14/56/111456fd7f0aa0848c5cf7698eda9272.jpg"
           alt="Gallery Image"
         />
         <img
           className="w-full size-40 object-cover"
-          src="https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80"
+          src="https://i.pinimg.com/736x/dd/00/33/dd003383f50e3c1e1b5f47c00882df39.jpg"
           alt="Gallery Image"
         />
         <img
           className="w-full size-40 object-cover"
-          src="https://images.unsplash.com/photo-1586232702178-f044c5f4d4b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80"
+          src="https://i.pinimg.com/736x/8f/cb/8b/8fcb8bc85ff81f5873832dda4c549ff5.jpg"
           alt="Gallery Image"
         />
         <img
           className="w-full size-40 object-cover"
-          src="https://images.unsplash.com/photo-1542125387-c71274d94f0a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80"
+          src="https://i.pinimg.com/736x/3e/48/bb/3e48bbadae3c255d043a1e7c9ac397d6.jpg"
           alt="Gallery Image"
         />
         <img
           className="w-full size-40 object-cover"
-          src="https://images.unsplash.com/photo-1668869713519-9bcbb0da7171?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80"
+          src="https://i.pinimg.com/736x/7e/5f/e8/7e5fe8208412d8a8e27a34dacb03bb87.jpg"
           alt="Gallery Image"
         />
         <img
           className="w-full size-40 object-cover"
-          src="https://images.unsplash.com/photo-1668584054035-f5ba7d426401?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80"
+          src="https://i.pinimg.com/736x/9a/a5/b7/9aa5b70f3a087f26bcbe3fe23c8ff1ea.jpg"
           alt="Gallery Image"
         />
         <img
           className="w-full size-40 object-cover"
-          src="https://images.unsplash.com/photo-1668863699009-1e3b4118675d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80"
+          src="https://i.pinimg.com/736x/40/c6/78/40c6788c14c28714771b5ca451985778.jpg"
           alt="Gallery Image"
         />
       </div>
